4b535ce71fb4bd06b756e1789f944ac8a03fee8e
Tal Ater <tal@talater.com>
2014-09-09 19:39:46 +0300

    Added Jasmine

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,13 +2,25 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+    jasmine: {
+      dist: {
+        src: 'src/*.js',
+        options: {
+          specs: 'test/spec/spec.js',
+          outfile: 'test/SpecRunner.html'
+        }
+      }
+    }
   });
+
+  // Load NPM Tasks
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
 
   // Default task(s).
   grunt.registerTask('default', []);
 
   // Test task
-  grunt.registerTask('test', []);
+  grunt.registerTask('test', ['jasmine']);
 
 };
