d97b71f3d34904ec7a68bc7e7020428d17d09d5a
Tal Ater <tal@talater.com>
2014-09-09 19:53:02 +0300

    Added basic locus.js file. Added UglifyJS and jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,52 @@
 module.exports = function(grunt) {
+  "use strict";
 
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'src/locus.js'
+      ],
+      options: {
+        "node"      : true,
+        "browser"   : true,
+        "devel"     : false,
+        "camelcase" : true,
+        "curly"     : true,
+        "latedef"   : true,
+        "unused"    : true,
+        "trailing"  : true,
+        "eqeqeq"    : true,
+        "eqnull"    : true,
+        "evil"      : false,
+        "forin"     : true,
+        "immed"     : true,
+        "laxbreak"  : false,
+        "newcap"    : true,
+        "noarg"     : true,
+        "noempty"   : false,
+        "nonew"     : true,
+        "onevar"    : false,
+        "plusplus"  : false,
+        "undef"     : true,
+        "sub"       : true,
+        "strict"    : true,
+        "white"     : false,
+        "indent"    : 2
+      }
+    },
+    uglify: {
+      options: {
+        preserveComments: 'some'
+      },
+      all: {
+        files: {
+          'dist/locus.min.js': ['src/locus.js']
+        }
+      }
+    },
     jasmine: {
       dist: {
         src: 'src/*.js',
@@ -15,12 +59,14 @@ module.exports = function(grunt) {
   });
 
   // Load NPM Tasks
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
 
   // Default task(s).
-  grunt.registerTask('default', []);
+  grunt.registerTask('default', ['jshint', 'uglify']);
 
   // Test task
-  grunt.registerTask('test', ['jasmine']);
+  grunt.registerTask('test', ['jshint', 'jasmine']);
 
 };
---
+++ src/locus.js
@@ -0,0 +1,13 @@
+//! locus
+//! version : 0.1.0
+//! author  : Tal Ater @TalAter
+//! license : MIT
+//! https://github.com/TalAter/locus
+
+(function (undefined) {
+  "use strict";
+
+  // Save a reference to the global object (window in the browser)
+  // var root = this;
+
+}).call(this);
