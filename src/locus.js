8c93cdea6a122912c46f9af84a6ec4b22de9cd85
Tal Ater <tal@talater.com>
2014-09-09 21:50:51 +0300

    Added addFences() & getFences(). Added more testing

--- src/locus.js
+++ src/locus.js
@@ -8,6 +8,45 @@
   "use strict";
 
   // Save a reference to the global object (window in the browser)
-  // var root = this;
+  var _root = this;
+
+  // Holds all known fences
+  var _fences = {};
+
+  // Expose functionality
+  _root.Locus = {
+
+    //@TODO: Describe object in more detail
+    /**
+     * add new fences to Locus
+     *
+     * ### Examples:
+     *
+     *     var fences = {'Moscone': {
+     *      'coord': '37.783944,-122.401289',
+     *      'radius': '300m',
+     *      'fn': function() {}
+     *     }};
+     *
+     *     locus.addFences(fences);
+     *
+     * @param {Object} fences - fences to add
+     * @method addFences
+     */
+    addFences: function(fences) {
+      _fences = fences;
+    },
+
+    //@TODO: Describe object returned in more detail
+    /**
+     * Returns all currently registered fences
+     *
+     * @method addFences
+     * @return {Object} fences
+     */
+    getFences: function() {
+      return _fences;
+    }
+  };
 
 }).call(this);
--- test/spec/spec.js
+++ test/spec/spec.js
@@ -1,16 +1,54 @@
-//TODO: Add tests!
-describe('Tests', function() {
+describe('Locus', function() {
   'use strict';
 
-  beforeEach(function() {
-
+  it('should exist in global namespace', function () {
+    expect(window.Locus).toBeDefined();
   });
 
-  afterEach(function() {
-
+  it('should contain addFences method', function () {
+    expect(window.Locus.addFences).toBeDefined();
   });
 
-  it('should not give an error because we have 0% test coverage ;-)', function() {
-    expect(true).toBe(true);
+  it('should contain getFences method', function () {
+    expect(window.Locus.getFences).toBeDefined();
+  });
+
+});
+
+describe('Locus.getFences', function() {
+  'use strict';
+
+  it('should return an object', function() {
+    var knownFences = Locus.getFences();
+    expect(typeof knownFences).toBe('object');
+    expect(Object.keys(knownFences).length).toBe(0);
+  });
+
+});
+
+describe('Locus.addFences', function() {
+  'use strict';
+
+  it('should add fences when passed an object containing a few fences', function() {
+    var knownFences;
+
+    knownFences = Locus.getFences();
+    expect(knownFences['Azrieli']).not.toBeDefined();
+
+    Locus.addFences({
+      'Azrieli': {
+        'coord': '32.074194, 34.791949',
+        'fn': function() {}
+      },
+      'Moscone': {
+        'coord': '37.783944,-122.401289',
+        'radius': '300m',
+        'fn': function() {}
+      }
+    });
+
+    knownFences = Locus.getFences();
+    expect(knownFences['Azrieli']).toBeDefined();
+    expect(Object.keys(knownFences).length).toBe(2);
   });
 });
