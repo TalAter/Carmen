8be68e973056cdb58007965bccbaab367828d2f5
Tal Ater <tal@talater.com>
2014-09-09 23:20:10 +0300

    Added some pending specs for location matching

---
+++ test/spec/LocatingSpec.js
@@ -0,0 +1,24 @@
+(function() {
+  var testFences = {
+    'Azrieli': {
+      'coord': '32.074194, 34.791949',
+      'fn': function() {}
+    },
+    'Moscone': {
+      'coord': '37.783944,-122.401289',
+      'radius': '300m',
+      'fn': function() {}
+    }
+  };
+
+  describe('Location matching', function() {
+    'use strict';
+
+    xit('should not match when outside a fence', function () {
+    });
+
+    xit('should match when inside a fence', function () {
+    });
+
+  });
+})();
