3ba084cd408bcf241952030c94309dc48737e546
Tal Ater <tal@talater.com>
2014-09-09 23:45:52 +0300

    Fixed fence fixtures

--- test/spec/BasicSpec.js
+++ test/spec/BasicSpec.js
@@ -1,16 +1,34 @@
 (function() {
-  var testFences = {
-    'Vertical': {
-      'coord': '39.456179, -0.346079',
+  var _threeFences = {
+    'Rock of Gibraltar': {
+      'coord': '36.125833, -5.343056',
       'fn': function() {}
     },
-    'Azrieli': {
-      'coord': '32.074194, 34.791949',
+    'Ernest Hemingway House': {
+      'coord': '24.551130, -81.800790',
       'fn': function() {}
     },
-    'Moscone': {
-      'coord': '37.783944,-122.401289',
+    'Fort Knox': {
+      'coord': '37.915499, -85.956172',
       'radius': '300m',
+      'fn': function() {}
+    }
+  };
+
+  var _oneFence = {
+    'Hagia Sophia': {
+      'coord': '41.008580, 28.980176',
+      'fn': function() {}
+    }
+  };
+
+  var _twoFences = {
+    'Sugarloaf Mountain': {
+      'coord': '-22.949932, -43.155149',
+      'fn': function() {}
+    },
+    'Millennium Dome': {
+      'coord': '51.502817, 0.003117',
       'fn': function() {}
     }
   };
@@ -56,16 +74,24 @@
 
     it('should add fences when passed an object containing a few fences', function() {
       var knownFences = Locus.getFences();
-      expect(knownFences['Azrieli']).not.toBeDefined();
+      expect(knownFences['Ernest Hemingway House']).not.toBeDefined();
 
-      Locus.addFences(testFences);
+      Locus.addFences(_threeFences);
 
       knownFences = Locus.getFences();
-      expect(knownFences['Azrieli']).toBeDefined();
+      expect(knownFences['Ernest Hemingway House']).toBeDefined();
       expect(Object.keys(knownFences).length).toBe(3);
     });
 
-    xit('should add fences without deleting previously added fences');
+    it('should add fences without deleting previously added fences', function() {
+      var knownFences = Locus.getFences();
+      expect(Object.keys(knownFences).length).toBe(0);
+
+      Locus.addFences(_threeFences);
+      expect(Object.keys(knownFences).length).toBe(3);
+
+
+    });
 
     xit('should overwrite previously added fences with same name');
   });
@@ -74,7 +100,7 @@
     'use strict';
 
     beforeEach(function() {
-      Locus.addFences(testFences);
+      Locus.addFences(_threeFences);
     });
 
     afterEach(function() {
@@ -83,47 +109,47 @@
 
     it('should remove all fences passed to it by name in the first argument as an array', function() {
       var knownFences = Locus.getFences();
-      expect(knownFences['Azrieli']).toBeDefined();
-      expect(knownFences['Moscone']).toBeDefined();
-      expect(knownFences['Vertical']).toBeDefined();
+      expect(knownFences['Ernest Hemingway House']).toBeDefined();
+      expect(knownFences['Fort Knox']).toBeDefined();
+      expect(knownFences['Rock of Gibraltar']).toBeDefined();
       expect(Object.keys(knownFences).length).toBe(3);
 
-      Locus.removeFences(['Azrieli', 'Vertical']);
+      Locus.removeFences(['Ernest Hemingway House', 'Rock of Gibraltar']);
 
       knownFences = Locus.getFences();
-      expect(knownFences['Azrieli']).not.toBeDefined();
-      expect(knownFences['Vertical']).not.toBeDefined();
-      expect(knownFences['Moscone']).toBeDefined();
+      expect(knownFences['Ernest Hemingway House']).not.toBeDefined();
+      expect(knownFences['Rock of Gibraltar']).not.toBeDefined();
+      expect(knownFences['Fort Knox']).toBeDefined();
       expect(Object.keys(knownFences).length).toBe(1);
     });
 
     it('should remove one fence passed to it by name in the first argument as a string', function() {
       var knownFences = Locus.getFences();
-      expect(knownFences['Azrieli']).toBeDefined();
-      expect(knownFences['Moscone']).toBeDefined();
-      expect(knownFences['Vertical']).toBeDefined();
+      expect(knownFences['Ernest Hemingway House']).toBeDefined();
+      expect(knownFences['Fort Knox']).toBeDefined();
+      expect(knownFences['Rock of Gibraltar']).toBeDefined();
       expect(Object.keys(knownFences).length).toBe(3);
 
-      Locus.removeFences('Azrieli');
+      Locus.removeFences('Ernest Hemingway House');
 
       knownFences = Locus.getFences();
-      expect(knownFences['Azrieli']).not.toBeDefined();
-      expect(knownFences['Vertical']).toBeDefined();
-      expect(knownFences['Moscone']).toBeDefined();
+      expect(knownFences['Ernest Hemingway House']).not.toBeDefined();
+      expect(knownFences['Rock of Gibraltar']).toBeDefined();
+      expect(knownFences['Fort Knox']).toBeDefined();
       expect(Object.keys(knownFences).length).toBe(2);
     });
 
     it('should remove all fences when called with no arguments', function() {
       var knownFences = Locus.getFences();
-      expect(knownFences['Azrieli']).toBeDefined();
-      expect(knownFences['Moscone']).toBeDefined();
+      expect(knownFences['Ernest Hemingway House']).toBeDefined();
+      expect(knownFences['Fort Knox']).toBeDefined();
       expect(Object.keys(knownFences).length).toBe(3);
 
       Locus.removeFences();
 
       knownFences = Locus.getFences();
-      expect(knownFences['Azrieli']).not.toBeDefined();
-      expect(knownFences['Moscone']).not.toBeDefined();
+      expect(knownFences['Ernest Hemingway House']).not.toBeDefined();
+      expect(knownFences['Fort Knox']).not.toBeDefined();
       expect(Object.keys(knownFences).length).toBe(0);
     });
   });
