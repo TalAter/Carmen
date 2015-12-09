f961749e169ff0b050c74353d4f59f2edde4f18f
Tal Ater <tal@talater.com>
2015-12-09 16:35:14 -0500

    Updated to grunt-contrib-uglify and fixed it's settings for a bug(?) in that version

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function(grunt) {
     },
     uglify: {
       options: {
-        preserveComments: 'some'
+        preserveComments: /^\! /
       },
       all: {
         files: {
